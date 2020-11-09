4d8998f097e45edbdb7cfc2aa65d1580d28c21e4
lixo <39702577+lixo-6c69786f@users.noreply.github.com>
2020-11-09 09:35:23 +0100

    Update RunApplications.java
    idk anders halt

--- src/net/domih/RunApplications.java
+++ src/net/domih/RunApplications.java
@@ -6,40 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RunApplications {
-	
-	public static List<File> files = new ArrayList<File>();
-	public static List<File> ghostFiles = new ArrayList<File>();
-	
-	public static void getFiles(String dir) {
+
+	public static List<File> getExecutables(String dir) {
+		List<File> files = new ArrayList<File>();
+
 		File directory = new File(dir);
 		File[] fileList = directory.listFiles();
-	    if(fileList != null) {
-	        for (File file : fileList) {      
-	            if (file.isFile() && file.getAbsolutePath().endsWith(".exe")) {
-	                files.add(file);
-	            } else if (file.isDirectory()) {
-	                getFiles(file.getAbsoluteFile().toString());
-	            }
-	        }
-	    }
+
+		if (fileList != null) {
+
+			for (File file : fileList) {
+				if (file.isFile() && file.getAbsolutePath().endsWith(".exe")) {
+					files.add(file);
+				} else if (file.isDirectory()) {
+					files.addAll(getExecutables(file.getAbsoluteFile().toString()));
+				}
+			}
+		}
+
+		return files;
 	}
 
-	public static void runApplication() {
+	public static void runApplication(List<File> files) {
 		for (File file : files) {
 			try {
-				if (!ghostFiles.contains(file)) {
-					Runtime.getRuntime().exec(file.getAbsolutePath().toString(), null, new File("."));
-					ghostFiles.add(file);
-				}
+				Runtime.getRuntime().exec(file.getAbsolutePath().toString(), null, new File("."));
 			} catch (IOException e) {
-				ghostFiles.add(file);
-				runApplication();
+				// probably no permission
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		getFiles("C:\\");
-		runApplication();
+		List<File> files = getExecutables("C:\\");
+		runApplication(files);
 	}
 }
